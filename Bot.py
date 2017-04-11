035695d05e893750718ce8c9268e5cfb1a6278c7
VamsiMohanRamineedi <vmr286@nyu.edu>
2017-04-10 20:11:40 -0400

    Changed screen name to boolean

--- Bot.py
+++ Bot.py
@@ -9,3 +9,20 @@ descriptio_list = list(dataset["description"])
 #dataset_new=dataset["has_extended_profile"].dropna()
 dataset_new = dataset[["screen_name","followers_count","friends_count","listedcount","favourites_count","verified","statuses_count","default_profile","default_profile_image","bot"]]
     
+X = dataset_new.iloc[:, :-1].values
+#y = dataset_new.iloc[:, 9].values
+
+                
+# Preprocessing the screen name feature
+#dataset_new["screen_name_bool"]=0
+import re
+pattern=r"bot"
+for i in range(0, len(dataset_new)):
+    if re.search(pattern, X[i,0]):
+        dataset_new["screen_name"][i]=1
+        
+    else:
+        dataset_new["screen_name"][i]=0
+    X[i,0]= dataset_new["screen_name"][i]
+
+y = dataset_new.iloc[:, 9].values
