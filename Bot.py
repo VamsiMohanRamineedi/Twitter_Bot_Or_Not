0190f3bd7626fbdc55eaff2582daf1a204a3485f
VamsiMohanRamineedi <vmr286@nyu.edu>
2017-04-10 20:23:29 -0400

    commented unnecessary

--- Bot.py
+++ Bot.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv(r'C:\Users\ramin\OneDrive - nyu.edu\Spring 2017\Machine Learning\Project\train_data.csv', encoding="cp437")
-descriptio_list = list(dataset["description"])
+#descriptio_list = list(dataset["description"])
 #dataset_new=dataset["has_extended_profile"].dropna()
 dataset_new = dataset[["screen_name","followers_count","friends_count","listedcount","favourites_count","verified","statuses_count","default_profile","default_profile_image","bot"]]
     
